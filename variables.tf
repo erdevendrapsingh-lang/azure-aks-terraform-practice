ed971ad6099c521d88e775de1a398f0412f2e9d2
erdevendrapsingh-lang <er.devendrapsingh@gmail.com>
2026-09-15 10:16:44 +0530

    Define variables for Azure AKS configuration
    Added variables for resource group, location, AKS cluster, DNS prefix, node count, and VM size.

--- variables.tf
+++ variables.tf
@@ -1,1 +1,35 @@
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+  default     = "rg-aks-practice"
+}
 
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "Central India"
+}
+
+variable "aks_cluster_name" {
+  description = "Name of the AKS cluster"
+  type        = string
+  default     = "aks-devops-practice"
+}
+
+variable "dns_prefix" {
+  description = "DNS prefix for AKS"
+  type        = string
+  default     = "aks-devops-practice"
+}
+
+variable "node_count" {
+  description = "Number of AKS nodes"
+  type        = number
+  default     = 1
+}
+
+variable "vm_size" {
+  description = "VM size for AKS nodes"
+  type        = string
+  default     = "Standard_B2s_v2"
+}
