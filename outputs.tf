6f057cbfdb38a26631b698b3a848f5e6c53d1b83
erdevendrapsingh-lang <er.devendrapsingh@gmail.com>
2026-09-15 10:19:17 +0530

    Add outputs for Azure Resource Group and AKS cluster

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,19 @@
+output "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  value       = azurerm_resource_group.aks_rg.name
+}
 
+output "aks_cluster_name" {
+  description = "Name of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.aks.name
+}
+
+output "aks_kubernetes_version" {
+  description = "Kubernetes version of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.aks.kubernetes_version
+}
+
+output "aks_fqdn" {
+  description = "Fully Qualified Domain Name of the AKS API server"
+  value       = azurerm_kubernetes_cluster.aks.fqdn
+}
